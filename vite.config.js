8d75408d6ce71b5fd21b2c41265f397e38cfe960
medmodroussi-ops <medmodroussi@gmail.com>
2026-03-09 23:43:17 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite'; // Changement ici : 'vite' au lieu de 'vitest/config'
 
 export default defineConfig({
   plugins: [sveltekit()],
   test: {
+    // Vitest supporte parfaitement le defineConfig de Vite
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
 });
